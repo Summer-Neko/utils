038f8b28ee1eae1d32d2c56241b8721e50bb20d5
Summer-Neko <summernekoo@gmail.com>
2024-11-20 19:11:52 +0800

    Update fetchGenshinWishLink.js

--- GetUrl/fetchGenshinWishLink.js
+++ GetUrl/fetchGenshinWishLink.js
@@ -1,14 +1,9 @@
 module.exports = {
-    // 更新缓存文件版本
-    getCacheVersion: () => "2.31.0.0",
-
-    // 从日志文件中提取游戏路径
+    getCacheVersion: () => "2.31.0.0",   // 更新文件夹
     extractGameDir: (logContent) => {
         const match = logContent.match(/([A-Z]:\\.+?\\(GenshinImpact_Data|YuanShen_Data))/);
         return match ? match[1] : null;
     },
-
-    // 从缓存文件中提取祈愿记录链接
     extractGachaLogUrl: (cachePath) => {
         const fs = require("fs");
         const url = require("url");
@@ -25,7 +20,6 @@ module.exports = {
         }
         return null;
     },
-
     simplifyUrl: (rawUrl) => {
         const url = require("url");
         const parsed = url.parse(rawUrl, true);
